32784e259cc1f0d91af50615a32ecf09e6334063
Shubham Gaikwad <gaikwadshubham.work@gmail.com>
2026-01-17 15:29:44 +0530

    init new

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white text-4xl font-bold">
+        NativeWind is working 🚀
+      </Text>
+    </View>
+  );
+}
